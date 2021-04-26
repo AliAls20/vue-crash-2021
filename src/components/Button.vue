1e15a2eaacd325bfbe6966c8f47d08af5c7d3320
Ali <ali@compete.nl>
2021-04-26 19:34:44 +0200

    Added Button and Tasks components

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click="onClick" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: {
+      type: String,
+      default: "Button Text",
+    },
+    color: {
+      type: String,
+      default: "green",
+    },
+  },
+  methods: {
+    onClick() {
+      console.log("click");
+    },
+  },
+};
+</script>
