a86b5330585f943cecc4b4f407f8e5ee6f4917d0
Ali <ali@compete.nl>
2021-04-26 19:55:34 +0200

    added task component

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,10 +1,11 @@
 <template>
   <div v-for="task in tasks" :key="task.id">
-    <h3>{{ task.text }}</h3>
+    <Task :task="task" />
   </div>
 </template>
 
 <script>
+import Task from "./Task";
 export default {
   name: "Tasks",
   props: {
@@ -12,6 +13,9 @@ export default {
       type: Array,
       default: [],
     },
+  },
+  components: {
+    Task,
   },
 };
 </script>
